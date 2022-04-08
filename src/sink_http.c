08e7e7912a4bb34c4efe457df18b096dde9af182
Michael Goldsby <mgoldsby@lyft.com>
2022-04-08 14:26:33 -0700

    one less log line

--- src/sink_http.c
+++ src/sink_http.c
@@ -141,7 +141,6 @@ static int add_metrics(void* data, metric_type type, char* name, void* value) {
 
         /* We have no matches and should skip */
         if (rc < 0) {
-            syslog(LOG_NOTICE, "Dropped non-matching metric: %s", name);
             return 0;
         }
         syslog(LOG_NOTICE, "Passed matching metric: %s", name);
