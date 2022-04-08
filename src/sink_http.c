a9cf1d9738a564c97dee75405cd3d500296ff61d
Michael Goldsby <mgoldsby@lyft.com>
2022-04-08 14:10:41 -0700

    log_notice

--- src/sink_http.c
+++ src/sink_http.c
@@ -141,10 +141,10 @@ static int add_metrics(void* data, metric_type type, char* name, void* value) {
 
         /* We have no matches and should skip */
         if (rc < 0) {
-            syslog(LOG_DEBUG, "Dropped non-matching metric: %s", name);
+            syslog(LOG_NOTICE, "Dropped non-matching metric: %s", name);
             return 0;
         }
-        syslog(LOG_DEBUG, "Passed matching metric: %s", name);
+        syslog(LOG_NOTICE, "Passed matching metric: %s", name);
     }
 
     if (json_object_size(info->jobjects[0]) > MAX_BODY_OBJECTS) {
