d509df365270454f7f47d2cc0ca19aa5dcfbb919
jmarchioni <jason.marchioni@loblaw.ca>
2022-12-14 12:27:11 -0500

    sum->count

--- src/sink_stream.c
+++ src/sink_stream.c
@@ -38,7 +38,7 @@ static int stream_formatter(FILE *pipe, void *data, metric_type type, char *name
             if (ct->global_config->extended_counters) {
                 //STREAM("%s%s.count|%" PRIu64 "|%lld\n", prefix, name, counter_count(value));
                 //STREAM("%s%s.mean|%f|%lld\n", prefix, name, counter_mean(value));
-                //STREAM("%s%s.sum|%f|%lld\n", prefix, name, counter_sum(value));
+                STREAM("%s%s.count|%f|%lld\n", prefix, name, counter_sum(value));
                 //STREAM("%s%s.lower|%f|%lld\n", prefix, name, counter_min(value));
                 //STREAM("%s%s.upper|%f|%lld\n", prefix, name, counter_max(value));
                 STREAM("%s%s.rate|%f|%lld\n", prefix, name, counter_sum(value) / ct->global_config->flush_interval);
